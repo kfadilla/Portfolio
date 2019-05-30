495e64223e3db53c574ec39cb7894969917b8579
Michael Fadillah <fadillahmichael@hotmail.com>
2019-05-29 19:19:08 -0700

    Updates

---
+++ precache-manifest.0b8fe515eed2d59cfea4078dbfed7234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf8b53478029489aa17ba5fd5df3e44",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2f060523a70092205c77",
+    "url": "/Portfolio/static/css/main.ca00a796.chunk.css"
+  },
+  {
+    "revision": "a6401c76a731a674aeb2",
+    "url": "/Portfolio/static/js/2.68d140fa.chunk.js"
+  },
+  {
+    "revision": "2f060523a70092205c77",
+    "url": "/Portfolio/static/js/main.8839d3bb.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "4956bb558585531312ecc677d0ace9eb",
+    "url": "/Portfolio/static/media/grad.4956bb55.jpg"
+  },
+  {
+    "revision": "6bf1a9efd864fd3cdada8d4d32fd7a4b",
+    "url": "/Portfolio/static/media/head.6bf1a9ef.png"
+  }
+]);
